17660ea1cd284c9f3981c805261283d1a116c930
Yami Odymel <yamiodymel@gmail.com>
2017-07-24 23:07:32 +0800

    base

--- main.go
+++ main.go
@@ -1,193 +1,178 @@
 package tavern
 
-import "errors"
+import "strconv"
 
-func Add(value interface{}) *Tavern {
-
-}
-
-type E struct {
-	Min      error
-	Max      error
-	Length   error
-	Range    error
-	Email    error
-	URL      error
-	IP       error
-	Date     error
-	In       error
-	RegExp   error
-	Equal    error
-	Required error
+func Add() *Tavern {
+	return &Tavern{}
 }
 
 type Tavern struct {
 	rules []rule
 }
 
-type rule struct {
-	value       interface{}
-	intValue    int
-	stringValue string
-	errors      E
-	typ         string
-	min         int
-	max         int
-	length      [2]int
-	numberRange [2]int
-	required    bool
-	isEmail     bool
-	urlFormat   []string
-	ipFormat    string
-	in          []interface{}
-	dateFormat  []string
-	regExp      []string
-	equal       interface{}
-}
-
-func (t *Tavern) latestRule() *rule {
+func (t *Tavern) lastRule() *rule {
 	return &t.rules[len(t.rules)-1]
 }
 
-func (r *rule) checkLength() error {
-	if len(r.length) == 0 {
-		return nil
-	}
-	if len(r.stringValue) < r.length[0] || len(r.stringValue) > r.length[0] {
-		if r.errors.Length != nil {
-			return r.errors.Length
-		}
-		return errors.New("The length is incorrect.")
-	}
-	return nil
-}
-
-func (r *rule) checkRange() error {
-	if r.typ != "int" {
-		return nil
-	}
-	if r.intValue < r.numberRange[0] || r.intValue > r.numberRange[1] {
-		if r.errors.Range != nil {
-			return r.errors.Range
-		}
-		return errors.New("The number doesn't in the range.")
-	}
-	return nil
-}
-
-func (r *rule) checkMinMax() error {
-	if r.min == 0 {
-		return nil
-	}
-}
-
-func (r *rule) assert() {
-	switch r.value.(type) {
-	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+func (t *Tavern) Add(value interface{}) *Tavern {
+	r := rule{}
+	switch v := value.(type) {
+	case int:
 		r.typ = "int"
+		r.intValue = v
+		r.stringValue = strconv.Itoa(v)
+	case int8:
+		r.typ = "int"
+		r.intValue = int(v)
+		r.stringValue = strconv.Itoa(int(v))
+	case int16:
+		r.typ = "int"
+		r.intValue = int(v)
+		r.stringValue = strconv.Itoa(int(v))
+	case int32:
+		r.typ = "int"
+		r.intValue = int(v)
+		r.stringValue = strconv.Itoa(int(v))
+	case int64:
+		r.typ = "int"
+		r.intValue = int(v)
+		r.stringValue = strconv.Itoa(int(v))
+	case uint:
+		r.typ = "int"
+		r.intValue = int(v)
+		r.stringValue = strconv.Itoa(int(v))
+	case uint8:
+		r.typ = "int"
+		r.intValue = int(v)
+		r.stringValue = strconv.Itoa(int(v))
+	case uint16:
+		r.typ = "int"
+		r.intValue = int(v)
+		r.stringValue = strconv.Itoa(int(v))
+	case uint32:
+		r.typ = "int"
+		r.intValue = int(v)
+		r.stringValue = strconv.Itoa(int(v))
+	case uint64:
+		r.typ = "int"
+		r.intValue = int(v)
+		r.stringValue = strconv.Itoa(int(v))
 	case string:
 		r.typ = "string"
+		r.intValue, _ = strconv.Atoi(v)
+		r.stringValue = v
 	}
-	panic("The value must be a int or a string.")
-}
-
-func (t *Tavern) Add(value interface{}) *Tavern {
-	t.rules = append(t.rules, rule{})
-	return t
-}
-
-func (t *Tavern) Min(min int) *Tavern {
-	r := t.latestRule()
-	r.min = min
-	return t
-}
-
-func (t *Tavern) Max(max int) *Tavern {
-	r := t.latestRule()
-	r.max = max
+	r.value = value
+	t.rules = append(t.rules, r)
 	return t
 }
 
 func (t *Tavern) Length(min, max int) *Tavern {
-	r := t.latestRule()
+	r := t.lastRule()
 	r.length = [2]int{min, max}
 	return t
 }
 
 func (t *Tavern) Range(min, max int) *Tavern {
-	r := t.latestRule()
-	r.numberRange = [2]int{min, max}
+	r := t.lastRule()
+	r.rangeList = [2]int{min, max}
+	return t
+}
+
+func (t *Tavern) Min(min int) *Tavern {
+	r := t.lastRule()
+	r.min = number{
+		value: min,
+		has:   true,
+	}
+	return t
+}
+
+func (t *Tavern) Max(max int) *Tavern {
+	r := t.lastRule()
+	r.max = number{
+		value: max,
+		has:   true,
+	}
+	return t
+}
+
+func (t *Tavern) Date(formats ...string) *Tavern {
+	r := t.lastRule()
+	r.date = formats
 	return t
 }
 
 func (t *Tavern) Email() *Tavern {
-	r := t.latestRule()
-	r.isEmail = true
-	return t
-}
-
-func (t *Tavern) URL(must ...[]string) *Tavern {
-	r := t.latestRule()
-	if len(must) == 1 {
-		r.urlFormat = must[0]
-	}
-	return t
-}
-
-func (t *Tavern) IP(format ...string) *Tavern {
-	r := t.latestRule()
-	if len(format) == 1 {
-		if format[0] != "v4" && format[0] != "v6" {
-			panic("The IP format should be `v4` or either `v6`.")
-		}
-		r.ipFormat = format[0]
-	}
-	return t
-}
-
-func (t *Tavern) Date(format ...string) *Tavern {
-	r := t.latestRule()
-	r.dateFormat = format
+	r := t.lastRule()
+	r.email = true
 	return t
 }
 
 func (t *Tavern) In(values ...interface{}) *Tavern {
-	r := t.latestRule()
+	r := t.lastRule()
 	r.in = values
 	return t
 }
 
-func (t *Tavern) RegExp(regexps ...string) *Tavern {
-	r := t.latestRule()
-	r.regExp = regexps
+func (t *Tavern) IP(typ ...string) *Tavern {
+	r := t.lastRule()
+	r.ip = typ
 	return t
 }
 
-func (t *Tavern) Equal(expect interface{}) *Tavern {
-	r := t.latestRule()
-	t.equal = compare
+func (t *Tavern) URL(contains ...string) *Tavern {
+	r := t.lastRule()
+	r.url = contains
+	return t
+}
+
+func (t *Tavern) Equal(compare interface{}) *Tavern {
+	r := t.lastRule()
+	r.equal = compare
+	return t
+}
+
+func (t *Tavern) RegExp(reg string) *Tavern {
+	r := t.lastRule()
+	r.regexp = reg
 	return t
 }
 
 func (t *Tavern) Required() *Tavern {
-	r := t.latestRule()
+	r := t.lastRule()
 	r.required = true
 	return t
 }
 
-func (t *Tavern) Error(e E) *Tavern {
-	r := t.latestRule()
-	r.errors = e
-	return t
-}
-
 func (t *Tavern) Check() error {
-	for _, v := range t.rules {
-		v.assert()
 
-	}
 }
 
 func (t *Tavern) CheckAll() []error {
 
+}
+
+type rule struct {
+	value       interface{}
+	stringValue string
+	intValue    int
+	required    bool
+	email       bool
+	url         []string
+	ip          []string
+	in          []interface{}
+	date        []string
+	rangeList   [2]int
+	length      [2]int
+	min         number
+	max         number
+	typ         string
+	equal       interface{}
+	regexp      string
+}
+
+type number struct {
+	value int
+	has   bool
 }
